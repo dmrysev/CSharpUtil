b7d3d89cd833ef4c5253dfa39505942c7b3976f8
dmrysev <dmrysev@protonmail.com>
2024-03-31 12:04:20 +0300

    Added Core.Seq

---
+++ Core/Seq.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpUtil.Core;
+
+class Seq {
+
+public static string joinSeqComma<T> (IEnumerable<T> seq) {
+    return string.Join(", ", seq);
+}
+
+public static string joinSeqNewLine<T> (IEnumerable<T> seq) {
+    return string.Join("\n", seq);
+}
+
+}
