70fec10e075958271a75c49d777e266086f0a57a
dmrysev <dmrysev@protonmail.com>
2024-04-04 13:28:16 +0300

    Refactored

--- Core/Seq.cs
+++ Core/Seq.cs
@@ -4,8 +4,6 @@ namespace CSharpUtil.Core;
 
 public static class IEnumerable {
 
-public static string JoinByComma<T> (this IEnumerable<T> seq) => string.Join(", ", seq);
-public static string JoinByNewLine<T> (this IEnumerable<T> seq) => string.Join("\n", seq);
 public static int CountItem<T>  (this IEnumerable<T> seq, T item) where T : IEquatable<T> =>
     seq
     .Aggregate(0, (total, next) => next.Equals(item) ? total + 1 : total);
