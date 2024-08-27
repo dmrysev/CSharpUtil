c0d2529de678b5550a6d05b383f27d0cf83be454
Dmitri Rõssev <dmitri.rossev@depowise.com>
2024-08-27 14:23:51 +0300

    Added HttpClientHandlerExtensions

---
+++ Core/HttpClientHandlerExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Net.Http;
+
+namespace Util
+{
+    public static class HttpClientHandlerExtensions
+    {
+        public static void AddCertificatesFromStore(this HttpClientHandler handler, StoreName storeName, StoreLocation storeLocation)
+        {
+            X509Store store = new X509Store(storeName, storeLocation);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+                X509Certificate2Collection fcollection = store.Certificates;
+                foreach (X509Certificate2 x509 in fcollection)
+                {
+                    handler.ClientCertificates.Add(x509);
+                }
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+    }
+
+}
